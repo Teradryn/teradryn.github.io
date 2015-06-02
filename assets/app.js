5e6c7a361472f53ee80981fedbfcce21101d0be5
Connor Desktop <connormccutcheon95@gmail.com>
2015-06-01 22:51:12 -0700

    adding in the team section for content to be writen

--- assets/app.js
+++ assets/app.js
@@ -4,52 +4,73 @@ var homePage = angular.module('MainApp', [
 
 
 // Main controller
-homePage.controller('HomePageCtrl', ['$scope', function($scope) {
+homePage.controller('HomePageCtrl', ['$scope', '$mdSidenav', '$mdUtil', function(scope, sidenav, mdUtil) {
 	// Coming Soon
-	$scope.comingSoon	= '/partials/coming-soon.html';
-	$scope.emailSignUp	= '/partials/email-sign-up.html';
-	$scope.splash		= '/partials/splash.html';
+	scope.comingSoon	= '/partials/coming-soon.html';
+	scope.emailSignUp	= '/partials/email-sign-up.html';
+	scope.splash		= '/partials/splash.html';
 	// Headers
-	$scope.splashHeader	= '/partials/headers/splash-header.html';
-	$scope.navHeader	= '/partials/headers/nav-header.html';
+	scope.splashHeader	= '/partials/headers/splash-header.html';
+	scope.navHeader	= '/partials/headers/nav-header.html';
 	// Sections
-	$scope.aboutSection	= '/partials/sections/about-section.html';
-	$scope.hubSection	= '/partials/sections/hub-section.html';
-	$scope.callToAction	= '/partials/sections/call-to-action.html';
+	scope.aboutSection	= '/partials/sections/about-section.html';
+	scope.hubSection	= '/partials/sections/hub-section.html';
+	scope.callToAction	= '/partials/sections/call-to-action.html';
+	scope.teamMembers	= '/partials/sections/team.html';
+
+	var contentRef = "";
+	scope.invokeSidebar = function(contentId) {
+		if(contentRef == contentId){
+			sidenav('right').close()
+			contentRef = "";
+		}else{
+			contentRef = contentId;
+			var content = angular.element('#'+contentId).html();
+			angular.element('#sidenav-content').html(content)
+			sidenav('right').open()
+		}
+	}
+	
+	scope.close = function() {
+		sidenav('right').close()
+			.then(function() {
+				console.log('closed');
+			});
+	}
 }]);
 
 
 // Coming soon controller, move to module in new js file.
-homePage.controller('ComingSoon', ['$scope', '$interval', function($scope, $interval) {
+homePage.controller('ComingSoon', ['$scope', '$interval', function(scope, $interval) {
 	// Map time to launch to scope variables
 	var releaseDay 		= moment('06-01-2015 00:00:00', 'MM-DD-YYYY HH:mm:ss');
 	var today			= moment();
 
-	$scope.days 	= releaseDay.diff(today, 'days');
-	$scope.hours 	= releaseDay.diff(today, 'hours') 	% 24; // 24 hours in a day
-	$scope.minutes	= releaseDay.diff(today, 'minutes') % 60; // 60 minutes in an hour
-	$scope.seconds	= releaseDay.diff(today, 'seconds') % 60; // 60 seconds in a minute
+	scope.days 	= releaseDay.diff(today, 'days');
+	scope.hours 	= releaseDay.diff(today, 'hours') 	% 24; // 24 hours in a day
+	scope.minutes	= releaseDay.diff(today, 'minutes') % 60; // 60 minutes in an hour
+	scope.seconds	= releaseDay.diff(today, 'seconds') % 60; // 60 seconds in a minute
 
 	// Timer function
 	function tickDown(){
 		// reduce seconds
-		if($scope.seconds > 0) {
-			$scope.seconds--;
+		if(scope.seconds > 0) {
+			scope.seconds--;
 		} else {
-		$scope.seconds = 60;
+		scope.seconds = 60;
 		// reduce minutes
-		if ($scope.minutes > 0) {
-			$scope.minutes--;
+		if (scope.minutes > 0) {
+			scope.minutes--;
 		} else {
-		$scope.minutes = 60;
+		scope.minutes = 60;
 		// reduce hours
-		if ($scope.hours > 0) {
-			$scope.hours--;
+		if (scope.hours > 0) {
+			scope.hours--;
 		} else {
-		$scope.hours = 24;
+		scope.hours = 24;
 		// reduce days
-		if ($scope.days > 0) {
-			$scope.days--;
+		if (scope.days > 0) {
+			scope.days--;
 		}}}}
 	}
 	
@@ -57,14 +78,14 @@ homePage.controller('ComingSoon', ['$scope', '$interval', function($scope, $inte
 	$interval(tickDown, 1000);
 }]);
 
-homePage.controller('EmailSignUp', ['$scope', '$timeout', function($scope, $timeout) {
-	$scope.email = '';
-	$scope.status = 'waiting';
+homePage.controller('EmailSignUp', ['$scope', '$timeout', function(scope, $timeout) {
+	scope.email = '';
+	scope.status = 'waiting';
 
-	$scope.submit = function(e) {
+	scope.submit = function(e) {
 		// This is an example for css and html flow
-		$scope.status = 'sending';
-		$timeout(function() {$scope.status = 'sent'; console.log('sent')}, 1000);
+		scope.status = 'sending';
+		$timeout(function() {scope.status = 'sent'; console.log('sent')}, 1000);
 
 		// Todo Make an api call to add to email list.
 	}
