fc15a6a38f11d4f949a91a366f989ed0c69cd96e
Connor Desktop <connormccutcheon95@gmail.com>
2015-06-04 21:00:28 -0700

    update for brian

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ var homePage = angular.module('MainApp', [
 
 
 // Main controller
-homePage.controller('HomePageCtrl', ['$scope', '$mdSidenav', '$mdUtil', function(scope, sidenav, mdUtil) {
+homePage.controller('HomePageCtrl', ['$scope', '$mdSidenav', '$mdUtil', '$window', function(scope, sidenav, mdUtil, win) {
 	// Coming Soon
 	scope.comingSoon	= '/partials/coming-soon.html';
 	scope.emailSignUp	= '/partials/email-sign-up.html';
@@ -32,62 +32,18 @@ homePage.controller('HomePageCtrl', ['$scope', '$mdSidenav', '$mdUtil', function
 	}
 	
 	scope.close = function() {
-		sidenav('right').close()
-			.then(function() {
-				console.log('closed');
-			});
-	}
-}]);
-
-
-// Coming soon controller, move to module in new js file.
-homePage.controller('ComingSoon', ['$scope', '$interval', function(scope, $interval) {
-	// Map time to launch to scope variables
-	var releaseDay 		= moment('06-01-2015 00:00:00', 'MM-DD-YYYY HH:mm:ss');
-	var today			= moment();
-
-	scope.days 	= releaseDay.diff(today, 'days');
-	scope.hours 	= releaseDay.diff(today, 'hours') 	% 24; // 24 hours in a day
-	scope.minutes	= releaseDay.diff(today, 'minutes') % 60; // 60 minutes in an hour
-	scope.seconds	= releaseDay.diff(today, 'seconds') % 60; // 60 seconds in a minute
-
-	// Timer function
-	function tickDown(){
-		// reduce seconds
-		if(scope.seconds > 0) {
-			scope.seconds--;
-		} else {
-		scope.seconds = 60;
-		// reduce minutes
-		if (scope.minutes > 0) {
-			scope.minutes--;
-		} else {
-		scope.minutes = 60;
-		// reduce hours
-		if (scope.hours > 0) {
-			scope.hours--;
-		} else {
-		scope.hours = 24;
-		// reduce days
-		if (scope.days > 0) {
-			scope.days--;
-		}}}}
-	}
-	
-	// Call timer function per second
-	$interval(tickDown, 1000);
-}]);
-
-homePage.controller('EmailSignUp', ['$scope', '$timeout', function(scope, $timeout) {
-	scope.email = '';
-	scope.status = 'waiting';
-
-	scope.submit = function(e) {
-		// This is an example for css and html flow
-		scope.status = 'sending';
-		$timeout(function() {scope.status = 'sent'; console.log('sent')}, 1000);
-
-		// Todo Make an api call to add to email list.
+		sidenav('right').close();
+		contentRef = "";
 	}
 
+	scope.adjustTeamMembers = function() {
+		var teamMemebers = angular.element(document.getElementsByClassName('team-member'));
+		for (var i = 0; i < teamMemebers.length; i++) {
+			var el = angular.element(teamMemebers[i]);
+			el.css({height: el.width() + 'px'});
+		};
+	}
+
+	// scope.$watch(function() {}, scope.adjustTeamMembers);
+	angular.element(win).bind('resize', scope.adjustTeamMembers);
 }]);
--- assets/controllers/aboutSection.js
+++ assets/controllers/aboutSection.js
@@ -4,19 +4,19 @@ mainApp.controller('AboutSectionCtrl', ['$scope', function($scope){
 	$scope.aboutColumns = [
 
 		{
-			title:   "One",
-			image:   "/images/test.png",
-			content: "This is the first column in the main points sub section of the about section of the main website, you can see it towards the top."
+			title:   "Fast",
+			image:   "/images/fun.png",
+			// content: "All of our products, while created by skilled engineers"
 		},
 		{
-			title:   "Two",
-			image:   "/images/test.png",
-			content: "Please change the content that is in these columns they need to say something that is general enough about the company."
+			title:   "Fun",
+			image:   "/images/rocket.svg",
+			// content: "Please change the content that is in these columns they need to say something that is general enough about the company."
 		},
 		{
-			title:   "Three",
-			image:   "/images/test.png",
-			content: "Try to make all of the content the same length and keep the titles to one word or two small words. This column has two sentences."
+			title:   "Affordable",
+			image:   "/images/affordable.png",
+			// content: "Try to make all of the content the same length and keep the titles to one word or two small words. This column has two sentences."
 		}
 	];
 
